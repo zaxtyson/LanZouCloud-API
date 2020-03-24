c4861f81506cbb2eaf706eb75226a1e37ee9674e
zaxtyson <zaxtyson@qq.com>
2020-03-24 18:28:29 +0800

    fixed download crash

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r", encoding='utf8') as fh:
 
 setuptools.setup(
     name="lanzou-api",
-    version="2.4.0.1",
+    version="2.4.1",
     author="zaxtyson",
     author_email="zaxtyson@foxmail.com",
     description="LanZouCloud API",

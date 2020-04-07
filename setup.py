970b85031f01974ea98b175576ee9a908cbf9328
zaxtyson <zaxtyson@qq.com>
2020-04-07 16:33:07 +0800

    update version

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r", encoding='utf8') as fh:
 
 setuptools.setup(
     name="lanzou-api",
-    version="2.4.3",
+    version="2.4.4",
     author="zaxtyson",
     author_email="zaxtyson@foxmail.com",
     description="LanZouCloud API",
